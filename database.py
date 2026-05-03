c7931ddc07aa0843dc94e5faa12e2505664812c9
AliEyvazov <ali22342eyvazov@outlook.com>
2026-05-03 21:59:43 +0400

    database.py Environment Variable (os.getenv)

--- database.py
+++ database.py
@@ -1,9 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
-SQLALCEHMY_DATABASE_URL = "postgresql://postgres:admin22342@localhost/todo_db"
+SQLALCEHMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:admin22342@localhost/todo_db"
+)
+
 
 engine = create_engine(SQLALCEHMY_DATABASE_URL)
 
